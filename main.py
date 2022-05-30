f1500b8997bd38e55bc037962d3ac481cf83b03b
calvinhus <gjpcalvinho@gmail.com>
2022-05-30 14:18:24 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pickle
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+companies = pickle.load(open("companies.pkl", "rb"))
+api_home = "This API returns info about several companies. Go to /docs for more complete documentation on how to use it."
+
+
+@app.get("/")
+def home():
+    return api_home
+
+
+@app.get("/get-companies/{activity}")
+def get_companies(
+    activity: str = Path(
+        None,
+        description=f"The type of industry that company works in.\
+                      Can be one of the following: [texteis, couro, papel, borracha, metal]",
+    )
+):
+    return [(company["Nome"], company["Atividade"]) for company in companies[activity]]
